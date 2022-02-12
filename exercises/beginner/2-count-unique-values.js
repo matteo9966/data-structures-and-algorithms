4e697332ab983fa549a7b443591fdb880a3dfeeb
matteo9966 <matteoluigitommasi@gmail.com>
2022-02-12 16:05:25 +0100

    solved two problems

--- exercises/beginner/2-count-unique-values.js
+++ exercises/beginner/2-count-unique-values.js
@@ -2,26 +2,63 @@
 function countUniqueValues(arr = []) {
   let counter = 0; //the counter that keeps track
   for (let i = 0; i <= arr.length - 2; ) {
-    if (arr[i] != arr[i + 1]) { //if the adjacent are not the same a[i] is unique
-      if (i === arr.length - 2) { //if I reached the last and they are not the same,add one for a[i] and one for a[i+1]
+    if (arr[i] != arr[i + 1]) {
+      //if the adjacent are not the same a[i] is unique
+      if (i === arr.length - 2) {
+        //if I reached the last and they are not the same,add one for a[i] and one for a[i+1]
         counter++;
       }
       counter++;
     }
-    if (arr[i] === arr[i + 1]) { 
-      if (i === arr.length - 2) { //if i reached the end and i have i  
+    if (arr[i] === arr[i + 1]) {
+      if (i === arr.length - 2) {
+        //if i reached the end and i have i
         counter++;
       }
     }
 
     i++;
   }
-return counter
+  return counter;
 }
 
-const cout=countUniqueValues([-1, -1, 1, 2, 2,3,3]);
-console.log(cout);
+// const cout = countUniqueValues([-1, -1, 1, 2, 2, 3, 3]);
+// console.log(cout);
 
-function countUniqueValues2(){
-    //add alternative solution
+/* second way: */
+//two pointers, i and j
+//[-1,-1,1,2,2,3,4]
+// i   j             => a[i]==a[j] : j++
+//[-1,-1,1,2,2,3,4]
+//  i    j           => a[i]!=a[j] : i++ ,a[i]=a[j], j++
+//[-1,1,1,2,2,3,4]
+//    i   j          => a[i]!=a[j] : i++,a[i]=a[j], j++;
+//[-1,1,2,2,2,3,4]
+//      i   j        => a[i]==a[j] : j++;
+//[-1,1,2,2,2,3,4]
+//      i     j      => a[i]!=a[j] : i++,a[i]=a[j],j++;
+//[-1,1,2,3,2,3,4]
+//        i     j    => a[i]!=a[j] : i++,a[i]=a[j],j++;
+//[-1,1,2,3,4,3,4]
+//          i      j => j>length : return i+1 = 5
+
+function countUniqueValues2(arr = []) {
+  let i,j,count = 0;
+  for (i = 0, j = 1; j < arr.length; j++) {
+    if (arr[i] != [arr[j]]) {
+       i++;
+       arr[i]=arr[j]
+      }
+      count=i+1;
+  }
+  
+  return count;
 }
+
+
+console.log(countUniqueValues2([-1, -1])) // questo non lo considera
+console.log(countUniqueValues2([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32]))
+console.log(countUniqueValues2([-1, -1, 1, 2, 2, 3]))
+console.log(countUniqueValues2([-10,1,5,60,100,100,111]))
+console.log(countUniqueValues2([1,1,1,1,1,1,1,1,1,2]))
+
--- exercises/beginner/3-max-subarray.-sum.js
+++ exercises/beginner/3-max-subarray.-sum.js
@@ -8,5 +8,47 @@
  * @returns {Number} largest sum 
  */
 function maxSubarraySum(arr,n){
-
+ 
 }
+
+//something wrong
+
+/* 
+logic:
+example
+f(arr=[1,2,2,4,1,5],n=3): 
+
+maximum = a[0]+a[1]+a[2] = 5,
+tempSum = maximum = 5
+L = arr.length = 6
+i=0,
+j=3, //j=n
+
+---
+
+j<L  ? tempSum= tempSum - a[0] + a[j]
+3<6 ? tempSum=      5   - 1    + 4    =   8
+tempsum>maximum? maximum=tempsum => 8>5? maximum=8
+i++ i=1
+j++ j=4
+---
+tempSum= tempsum - a[1] + a[4] = 8 - 2 + 1 = 7
+7>8? no
+i++ i=2
+j++ j=5
+---
+tempSum = tempSum - a[2] + a[5] = 7 - 2 + 5 = 10
+10>8? maximum = 10
+
+i++
+j++ j=6
+---
+j>L : return maximum = 10 
+
+
+
+
+
+
+
+*/
