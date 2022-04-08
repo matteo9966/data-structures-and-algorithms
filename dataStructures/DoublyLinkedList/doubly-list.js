daee8959ed72bd2e24e0f5d38165aac37f24b6ec
matteo9966 <matteoluigitommasi@gmail.com>
2022-04-08 18:27:54 +0200

    reverse a doubly linkedlist

--- dataStructures/DoublyLinkedList/doubly-list.js
+++ dataStructures/DoublyLinkedList/doubly-list.js
@@ -119,20 +119,75 @@ class DoublyLinkedList{
         node.val=val;
         return node;
     }
+
+    insert(index,value){
+        if(index<0 || index>this.length) return
+        if(index==0){
+           return this.unshift(value)
+        }
+        if(index==this.length){
+            return this.push(value)
+        }
+        const newNode = new ListNode(value)
+        const node = this.get(index);
+        newNode.prev=node.prev;
+        node.prev.next=newNode;
+        newNode.next=node;
+        node.prev=newNode;
+        this.length=this.length+1;
+        return this
+    }
+
+  remove(index){
+      if(index<0 || index>this.length) return
+      if(index==0) return this.shift();
+      if(index==this.length-1) return this.pop()
+      const node = this.get(index);
+      const prev_node = node.prev;
+      const next_node = node.next;
+      prev_node.next = next_node;
+      next_node.prev=prev_node;
+      node.prev=null;
+      node.next=null;
+      this.length=this.length-1;
+      return node;
+  }
+
+  reverse(){
+    if(this.length<=1) return
+    let node = this.tail;
+    while(node){
+        const temp = node.next;
+        node.next=node.prev;
+        node.prev=temp;
+        node=node.next;
+    }
+    const temp = this.head;
+    this.head=this.tail;
+    this.tail=temp;
+  return this  
+}
+  
 }
 
 const list = new DoublyLinkedList();
-list.push("MI").push("CHIAMO").push("MATTEO")
+list.push("MI").push("CHIAMare").push("MATTEO").push("LUIGI").push("TOMMASI").reverse().reverse().reverse();
 
+// list.insert(1,'Voglio')
+// list.insert(2,'Dichiaratamente')
+// list.insert(5,'!!!!!')
+// list.insert(0,'!!!!!')
+// list.remove(2)
+// list.remove(2)
+// list.remove(2)
 // list.shift();
-list.unshift('DIRE')
-list.unshift('DEVO')
-list.unshift('PIACERE')
-console.log('-----')
-list.set(15,"PATATA");
-console.log({2:list.get(2)});
-console.log({4:list.get(4)});
+// list.unshift('DIRE')
+// list.unshift('DEVO')
+// list.unshift('PIACERE')
+// console.log('-----')
+// list.set(15,"PATATA");
+// console.log({2:list.get(2)});
+// console.log({4:list.get(4)});
+// console.log(list.length)
 
-
-console.log(list.length)
 list.print();
