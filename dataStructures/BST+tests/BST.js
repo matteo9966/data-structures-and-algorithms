9bcaed9ea646993d3b2060e5e14fa760cf48bd0c
matteo9966 <matteoluigitommasi@gmail.com>
2022-04-26 14:50:00 +0200

    added test to bst

--- dataStructures/BST+tests/BST.js
+++ dataStructures/BST+tests/BST.js
@@ -6,7 +6,7 @@ class BSTNode {
   }
 }
 
-exports.BST = class BST {
+ class BST {
   constructor() {
     this.root = null;
   }
@@ -38,9 +38,92 @@ exports.BST = class BST {
     }
   }
 
-  remove(value){
-    if(!value) return
-    if(this.root===null) return
+  remove(value) {
+    if (!value) return;
+    if (this.root === null) return null;
+
+    const findLargest = (node) => {
+      let largest = node;
+      while (largest.right) {
+        largest = largest.right;
+      }
+      return largest.value;
+    };
+
+    const removeHelper = (value, node) => {
+      if (!node) return null;
+
+      if (value < node.value) {
+       node.left = removeHelper(value, node.left);
+      }
+      else if (value > node.value) {
+        node.right = removeHelper(value, node.right);
+      }
+       else  {
+         if(!node.right && !node.left){
+           return null
+         }
+         else if(!node.left){
+           return node.right
+         }
+         else if(!node.right){
+           return node.left
+         }
+         else {
+           const largestOnLeft = findLargest(node.left);
+           node.left = removeHelper(largestOnLeft,node.left);
+           node.value = largestOnLeft;
+           return node
+         }
+     
+      }
+       
+      return node;
+
+    };
+    
+    this.root = removeHelper(value,this.root)
+    return this 
+
+  }
+
+  /**
+   * BreadthFirstSearch 
+   */
+  BFS(){
+    const queue = [];
+    const visited = [];
+    if(!this.root) return [];
+
+    queue.push(this.root)
+    
+    while(queue.length>0){
+      let visitedNode = queue.shift();
+      visitedNode.left && queue.push(visitedNode.left);
+      visitedNode.right && queue.push(visitedNode.right);
+      visited.push(visitedNode.value);
+    }
+    
+    return visited;
+
+
+
+  }
+
+  DFSPreOrder(){
     
   }
-}
+
+};
+
+module.exports.BST= BST;
+const bst = new BST();
+bst.insert(50);
+bst.insert(20);
+bst.insert(60);
+bst.insert(55);
+bst.insert(10);
+bst.insert(100);
+bst.remove(10);
+
+console.log(bst);
--- dataStructures/BST+tests/BST.test.js
+++ dataStructures/BST+tests/BST.test.js
@@ -39,3 +39,74 @@ test("passing multiple values returns a bst", () => {
 
   expect(bst).toEqual(copyBST);
 });
+
+test('removing from tree without elements returns undefined',()=>{
+  const bst = new BST();
+  const tree = bst.remove(10);
+  expect(tree).toBeNull();
+})
+
+test('removing a leaf that has no children',()=>{
+  const expectedBST = {
+    root: {
+      value: 50,
+      left: {
+        value: 20,
+        left: null,
+        right: null,
+      },
+      right: {
+        value: 60,
+        left: { value: 55, left: null, right: null },
+        right: { value: 100, left: null, right: null },
+      },
+    },
+  };
+  const bst = new BST();
+  bst.insert(50);
+  bst.insert(20);
+  bst.insert(60);
+  bst.insert(55);
+  bst.insert(10);
+  bst.insert(100);
+  bst.remove(10);
+  expect(bst).toEqual(expectedBST);
+
+})
+test('removing a leaf that has 1 child',()=>{
+  const expectedBST = {
+    root: {
+      value: 50,
+      left: {
+        value: 20,
+        left: { value: 10, left: null, right: null },
+        right: null,
+      },
+      right: { value: 100, left: null, right: null },
+    
+    },
+  };
+  const bst = new BST();
+  bst.insert(50);
+  bst.insert(20);
+  bst.insert(60);
+  bst.insert(10);
+  bst.insert(100);
+  bst.remove(60);
+  expect(bst).toEqual(expectedBST);
+
+})
+test('BFS with 5 nodes',()=>{
+
+  const expectedList = [50,20,60,10,100];
+  const bst = new BST();
+  bst.insert(50);
+  bst.insert(20);
+  bst.insert(60);
+  bst.insert(10);
+  bst.insert(100);
+  
+  expect(bst.BFS()).toEqual(expectedList);
+
+})
+
